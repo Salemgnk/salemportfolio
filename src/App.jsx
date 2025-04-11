9103cff4939bffa76a5dcfaae34a0c6810a5545c
Salemgnk <salem.gnandi@epitech.eu>
2025-04-11 23:36:18 +0100

    Creating the Home Page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Cyberpoet Portfolio</h1>
+      <p>Building something awesome...</p>
+    </div>
+  );
+}
+
+export default App;
