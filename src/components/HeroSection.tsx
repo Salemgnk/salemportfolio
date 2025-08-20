20395a76a6a3e89f1d1e371c1aadae5c6abaa14c
Salemgnk <salem.gnandi@epitech.eu>
2025-08-20 18:19:00 +0100

    feat: Creation of the second mode of the hero

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,17 +1,29 @@
 
 export default function HeroSection() {
-  return (
-    <section className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex flex-col items-center text-center gap-6">
+    const isDark = document.documentElement.classList.contains('dark');
 
-        {/* Image */}
-        <img
-          src="/src/assets/salem.jpeg"
-          alt="Salem GNANDI"
-          className="w-40 h-40 rounded-full object-cover shadow-2xl object-top"
-        />
-        <h1 className="text-5xl font-bold font-mono">Hi, I'm Salem GNANDI</h1>
-      </div>
-    </section>
-  );
+    return (
+        <section className="hero bg-primary min-h-screen">
+        <div className="hero-content flex flex-col items-center text-center gap-6">
+
+            {/* Image */}
+            <img
+            src="/src/assets/salem.jpeg"
+            alt={isDark ? "Salem GNANDI" : "Scorpi777"}
+            className="w-40 h-40 rounded-full object-cover shadow-2xl object-top"
+            />
+
+            <h1 className={`text-5xl font-bold ${isDark ? "font-mono text-green-400" : "font-serif text-black"}`}>
+                {isDark ? "Hi, I'm Scorpi777" : "Hi, I'm Salem GNANDI"}
+            </h1>
+
+            <p className={`"text-xl font-serif ${isDark ? "font-mono text-gray-300": "font-serif text-black"}`}>
+                {isDark ? "I'm a ctf enthusiast" :
+                "I'm a passionate software developer with a focus on building"}
+            </p>
+
+            <button className="btn btn-soft btn-primary">Contact me</button>
+        </div>
+        </section>
+    );
 }
